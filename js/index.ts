2960f70ed99257df83d1a95e4fe031feecea5781
Ralph de Horde <ralphdehorde@gmail.com>
2021-07-25 01:13:52 +0200

    Setup vite

--- js/index.ts
+++ js/index.ts
@@ -1,22 +1,3 @@
-// import * as _ from 'lodash';
-// // import wasm 
-// // import("../pkg").catch(console.error);
-// import { main_js } from "../pkg/rust_chip8_wasm";
-
-// main_js();
-
-// function component() {
-//     const element = document.createElement('div');
-
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//     return element;
-// }
-
-// document.body.appendChild(component());
-
-
-
 import App from './App.svelte';
 
 const app = new App({
